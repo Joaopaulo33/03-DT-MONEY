4bb884efb61e35f2f447e5f934cab3273ebcbfe3
joaopaulo33 <joaopaulo33573@gmail.com>
2023-01-23 20:54:31 -0300

    Criação contexto transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import { ThemeProvider } from 'styled-components'
-import reactLogo from './assets/react.svg'
-import { Transactions } from './pages/Transactions'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
-
+import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions/>
+
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
-
